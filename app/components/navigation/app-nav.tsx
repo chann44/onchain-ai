cfccc5d07e2cf9acd7aa178021c2a1c45f4e9824
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-12-07 21:20:58 +0530

    Added Wallet

--- app/components/navigation/app-nav.tsx
+++ app/components/navigation/app-nav.tsx
@@ -1,9 +1,31 @@
-import { Button} from "../ui/button";
+"use client";
+import { Button } from "../ui/button";
 import { SiteLogo } from "../logo/transparent";
+import { Wallet } from "../wallet";
+import { createSiweMessage } from "viem/siwe";
+import { useAccount, useSignMessage } from "wagmi";
 
 export function MainAppTopNav() {
-    return <nav className="h-16 mb-10 flex w-full bg-transparent justify-between items-center">
-        <SiteLogo className="h-6 w-6 stroke-primary" />
-        <Button >Connect Wallet</Button>
+  const { address, chainId } = useAccount();
+  const { signMessageAsync } = useSignMessage();
+  const onConnect = async () => {
+    const message = createSiweMessage({
+      address: address ?? ("" as `0x${string}`),
+      chainId: chainId ?? 0,
+      domain: "example.com",
+      nonce: "foobarbaz",
+      uri: "https://example.com/path",
+      version: "1",
+    });
+
+    const signedMessage = await signMessageAsync({ message });
+
+    return signedMessage;
+  };
+  return (
+    <nav className="h-16 mb-10 flex w-full bg-transparent justify-between items-center">
+      <SiteLogo className="h-6 w-6 stroke-primary" />
+      <Wallet onConnect={onConnect} />
     </nav>
+  );
 }
